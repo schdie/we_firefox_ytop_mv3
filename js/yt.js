5747b5719ef5e8286dd6ba380741f069e0373d29
schdie <4019285+schdie@users.noreply.github.com>
2026-06-01 15:25:08 -0300

    changed main webrequest client

--- js/yt.js
+++ js/yt.js
@@ -146,11 +146,11 @@ async function postJSON() {
       body: JSON.stringify({ "context":
 							{ "client":
 								{ 'clientName': 'ANDROID_VR',
-                'clientVersion': '1.71.26',
+                'clientVersion': '1.65.10',
                 'deviceMake': 'Oculus',
                 'deviceModel': 'Quest 3',
                 'androidSdkVersion': 32,
-                'userAgent': 'com.google.android.apps.youtube.vr.oculus/1.71.26 (Linux; U; Android 12L; eureka-user Build/SQ3A.220605.009.A1) gzip',
+                'userAgent': 'com.google.android.apps.youtube.vr.oculus/1.65.10 (Linux; U; Android 12L; eureka-user Build/SQ3A.220605.009.A1) gzip',
                 'osName': 'Android',
                 'osVersion': '12L',
                 'visitorData': VISITOR_DATA,
